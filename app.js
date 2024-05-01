4b2b06cdecaf9c284f506feeb73fcdd73864c89c
Dewangga <hamadakinose@gmail.com>
2024-05-01 20:56:14 +0700

    add deploy development

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const port = 3000;
 
 app.use(express.json());
 
-const v1 = require("./routes/v1/index");
-app.use("/api/v1", v1);
+app.get("/", (req, res) => {
+    res.json({
+        status: true,
+        message: "Hello World",
+        data: null
+    })
+})
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
